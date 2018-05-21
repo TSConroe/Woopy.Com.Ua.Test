f19dcbf2d7319e6f36a7864e3382ccf4b17a70c3
TSConroe <1conroe1@gmail.com>
2018-05-21 22:37:10 +0300

    Added SaleCheck Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,12 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.setProperty("webdriver.chrome.driver", "C://Chromedriver//chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.woopy.com.ua/");
-       //string Title = driver.getTitle();
+
+
 
                 }
     }
