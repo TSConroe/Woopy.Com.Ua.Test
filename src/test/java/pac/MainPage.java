6773c276dc7a1553f87699a6a3e55dde7b71a445
TSConroe <1conroe1@gmail.com>
2018-05-22 18:21:12 +0300

    Added two first points realization (not full)

---
+++ src/test/java/pac/MainPage.java
@@ -0,0 +1,25 @@
+package pac;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class MainPage  {
+
+    private  WebDriver driver;
+    String url = "https://www.woopy.com.ua";
+
+    public MainPage (WebDriver browser) {
+        this.driver = browser;
+        this.driver.manage().window().fullscreen();
+        PageFactory.initElements(browser, this);
+
+
+    }
+
+    public void Navigate(String url)
+    {
+        this.driver.navigate().to(this.url);
+    }
+}
+
---
+++ src/test/java/pac/MainTest.java
@@ -0,0 +1,82 @@
+package pac;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pac.MainPage;
+
+import java.util.List;
+import java.util.Random;
+
+
+public class MainTest {
+
+    public WebDriver driver;
+    final int CountOfTestSale = 3;
+    String url = "https://www.woopy.com.ua";
+
+    @BeforeTest
+    public void BeforeTest() {
+        System.setProperty("webdriver.chrome.driver", "C://Chromedriver//chromedriver.exe");
+        this.driver = new ChromeDriver();
+
+
+    }
+
+
+    @Test
+    public void testTitle() {
+       MainPage searchJeansPage = new MainPage(this.driver);
+       searchJeansPage.Navigate(url);
+        String Title = driver.getTitle();
+        String ExpectTitle = "Интернет магазин детской обуви Foksi (Woopy Orthopedic Украина, Minime, Tofino)";
+        Assert.assertEquals(Title, ExpectTitle);
+    }
+
+
+   @Test
+    public void SalePriceCheck() {
+       SalePage salePage = new SalePage(this.driver);
+       salePage.Navigate(url);
+       driver.get("https://www.woopy.com.ua/index.php/component/virtuemart/view/category/virtuemart_category_id/8");
+       String[] Links = salePage.GetOneProductSaleLink(CountOfTestSale);
+       for (int i = 0; i < CountOfTestSale; i++) {
+
+           System.out.println(Links[i]);
+           driver.get(Links[i]);
+           Assert.assertTrue(driver.findElement(By.className("old-price")).isDisplayed());
+
+       }
+   }
+
+    @Test
+    public void GirlsPriceCheck() {
+        GirlsPage girlsPage = new GirlsPage(this.driver);
+        girlsPage.Navigate(url);
+        driver.get("https://www.woopy.com.ua/index.php/component/virtuemart/view/category/virtuemart_category_id/7");
+        girlsPage.ClearFilter();
+        girlsPage.OpenAllProduct();
+
+    }
+
+
+
+
+    @AfterTest
+    public void AfterTest() {
+      //  driver.close();
+    }
+
+                       }
+
+
+
+
+
+
+
