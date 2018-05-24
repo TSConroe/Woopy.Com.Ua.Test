34304cd75450f3b0012638645910d66d2ec70e5f
TSConroe <1conroe1@gmail.com>
2018-05-24 23:01:17 +0300

    Refactoring - second transfer classes part

--- src/test/java/pac/PageObject/BoysPage.java
+++ src/test/java/pac/PageObject/BoysPage.java
@@ -1,14 +1,13 @@
 package pac.PageObject;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
 import java.util.ArrayList;
 import java.util.List;
-
-
 
 
 public class BoysPage {
@@ -37,11 +36,9 @@ public class BoysPage {
     public List<WebElement> orederNumber;
 
 
-    public List<WebElement> firstproductName;
-
     public BoysPage(WebDriver browser) {
         this.driver = browser;
-          this.driver.manage().window().maximize();
+        this.driver.manage().window().maximize();
         PageFactory.initElements(browser, this);
 
 
@@ -54,17 +51,13 @@ public class BoysPage {
 
     public int GetPageCount() {
 
-        List <WebElement> pageCount= driver.findElements(By.xpath("//*[@class=\"page-nav\"]//li"));
         int count = pageCount.size();
         return count;
     }
 
-    public void Navigate()
-    {
+    public void Navigate() {
         this.driver.navigate().to(this.url);
     }
-
-
 
 
     public void СhoiceFootwearType() {
@@ -75,43 +68,23 @@ public class BoysPage {
 
 
     }
-    public void СhoiceMoks( )
-        {
-            ClearFilter.click();
-            MoisksCheckBox.click();
-        }
-        public void СhoiceMoksAndSneakers( ) {
-            ClearFilter.click();
-            MoisksCheckBox.click();
-            SneakersCheckBox.click();
 
-        }
+    public void СhoiceMoks() {
+        ClearFilter.click();
+        MoisksCheckBox.click();
+    }
 
+    public void СhoiceMoksAndSneakers() {
+        ClearFilter.click();
+        MoisksCheckBox.click();
+        SneakersCheckBox.click();
 
-
-  /*  public List<WebElement> CountFootwer() {
-
-      //  firstproductName = driver.findElements(By.xpath("//*[@class=\"product-title\"]//a"));
-        System.out.println(firstproductName.size());
-    for (int i=0; i<pageCount.size()-1; i++)
-        {
-
-            firstproductName.addAll( driver.findElements(By.xpath("//*[@class=\"product-title\"]//a")));
-            ShowMoreButton.click();
-            System.out.println(firstproductName.size());
-
-        }
+    }
 
 
-       return firstproductName;
+    public void GetNumbersToNotSneakersShoes() {
 
-
-    }*/
-
-    public void GetNumbersToNotSneakersShoes()
-    {
-
-        for (int i = 0; i< productName.size(); i++
+        for (int i = 0; i < productName.size(); i++
                 ) {
 
             if (!"КРОССОВКИ".equalsIgnoreCase(productName.get(i).getText()))
@@ -119,18 +92,24 @@ public class BoysPage {
                 System.out.println(orederNumber.get(i).getText());
 
 
-            }
-            if (ShowMoreButton.isDisplayed())
-        ShowMoreButton.click();
         }
+        try {
+
+            ShowMoreButton.click();
+
+        } catch (NoSuchElementException e) {
+
+
+        }
+    }
+
+
 
     public List<String> GetProductList() {
 
-       // orederNumber =driver.findElements(By.xpath("//*[@class=\"order-number\"]//span"));
-        List <String> LinkList = new ArrayList<>();
+        List<String> LinkList = new ArrayList<>();
 
-        for (int i = 0; i < orederNumber.size(); i++)
-        {
+        for (int i = 0; i < orederNumber.size(); i++) {
             LinkList.add(orederNumber.get(i).getText());
 
         }
@@ -138,14 +117,7 @@ public class BoysPage {
         System.out.println(orederNumber.size() + "from boys page ");
         return LinkList;
 
-
-
-
     }
-
-
-
-
 
 
 }
